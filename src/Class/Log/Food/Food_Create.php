ad11da7db8a54fd81cae6a2c7f0fa9ee3c794b19
Elia <mail@eliareutlinger.ch>
2019-11-12 13:40:47 +0100

    Update Food_Create.php

--- src/Class/Log/Food/Food_Create.php
+++ src/Class/Log/Food/Food_Create.php
@@ -7,7 +7,11 @@ class Food_Create extends Food {
     public function create() {
 
         if ($this->date && $this->time) $stamp = $this->date." ".$this->time;
-        else $stamp = date('Y-m-d H:i');
+        else {
+            $stamp = date('Y-m-d H:i');
+            $this->date = date('Y-m-d', strtotime($stamp));
+            $this->time = date('H:i', strtotime($stamp));
+        };
 
         $stmt = Database::prepare("
             INSERT INTO ". self::$db_t_log_food . " 
